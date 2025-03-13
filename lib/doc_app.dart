e3fc6c058dad3c5213beeb29aeed1664649ea1f5
DESKTOP-EK3791T\RAMLINA <mino.fadio69@gmail.com>
2025-03-13 08:11:27 +0000

    Theminig color manager

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,5 +1,7 @@
 import 'package:advanced/core/routing/app_router.dart';
+import 'package:advanced/core/theming/colors_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -7,6 +9,20 @@ class DocApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: 'Doc Application',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: ColorsManager.mainBlue),
+      ),
+      /* builder: () => MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerDelegate: appRouter,
+        routeInformationParser: appRouter.defaultRouteParser(),
+      ),*/
+    );
   }
 }
