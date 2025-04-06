7d51893eefa8e5c686ca92fd3be4c3289292a5c3
DESKTOP-EK3791T\RAMLINA <mino.fadio69@gmail.com>
2025-04-06 13:11:55 +0000

    Add splash screen native

--- lib/core/theming/styles.dart
+++ lib/core/theming/styles.dart
@@ -1,4 +1,5 @@
 import 'package:advanced/core/theming/colors_manager.dart';
+import 'package:advanced/core/theming/font_weight_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,10 +15,20 @@ class TextStyles {
     fontWeight: FontWeight.bold,
     color: ColorsManager.mainBlue,
   );
+  static TextStyle font24BlueBold = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeightHelper.bold,
+    color: ColorsManager.mainBlue,
+  );
   static TextStyle font13GrayRegular = TextStyle(
     fontSize: 13.sp,
     fontWeight: FontWeight.normal,
-    color: Colors.grey,
+    color: ColorsManager.grey,
+  );
+  static TextStyle font14GrayRegular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.normal,
+    color: ColorsManager.grey,
   );
 
   static TextStyle font16WhiteSemiBold = TextStyle(
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:advanced/core/theming/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,8 +8,23 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: const Center(child: Text('Login Screen')),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Welcome Back', style: TextStyles.font24BlueBold),
+                Text(
+                  'Welcome Back.. Welcome Back.. Welcome Back.. Welcome Back.. Welcome Back.. Welcome Back',
+                  style: TextStyles.font14GrayRegular,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
